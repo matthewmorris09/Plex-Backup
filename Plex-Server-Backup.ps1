513c66fdb1b4a70b2bdc400e20d8c37ff680dde8
EolianPipes <matthewmorris09@gmail.com>
2017-02-12 15:52:21 -0500

    Added some echos to try out for next itme

--- Plex-Server-Backup.ps1
+++ Plex-Server-Backup.ps1
@@ -1,5 +1,9 @@
+# Let the user see that their Plex server is shutting down
+echo "Shutting off your Plex Server"
+
 # Shut down the Plex Server and prepare for backup
 Stop-Process -force -processname "Plex*"
+echo "Plex Server is off"
 
 # Finds 7zip to use in any directory
 if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"} 
@@ -14,6 +18,8 @@ $backupReg = $backupFolder + "Plex Media Server Registry Backup " + $date + ".re
 $plexServer = "C:\Users\Matthew\AppData\Local\Plex Media Server"
 $plexRegistry = "HKEY_CURRENT_USER\SOFTWARE\Plex, Inc.\Plex Media Server"
 
+echo "Creating a new folder for today's backup"
+
 # Make a new directory for the backup
 mkdir $backupFolder
 
@@ -21,8 +27,16 @@ mkdir $backupFolder
 # Zip the file / a = 'add' / mx = method and level of compression (9 is the highest or "Ultra" in 7z)
 7z a -mx=9 $backup7z $plexServer -xr!Cache
 
+echo "Plex Server backup complete!"
+echo "Time to make a copy of the Registry key"
+
 # Export Registry Key
 Reg export $plexRegistry $backupReg
 
+echo "Registry key is backed up!"
+echo "Turning back on the Plex Server"
+
 # Turn back on the Plex Server
 Start-Process -FilePath "C:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe"
+
+echo "Plex Server is a go!"
